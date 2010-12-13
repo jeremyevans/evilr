bd0f98774406427a5d1a2ac8778add51456229c2
Jeremy Evans <code@jeremyevans.net>
2010-12-13 12:14:45 -0800

    Object#share_singleton_class should handle singleton classes that don't already exist

--- ext/evilr/evilr.c
+++ ext/evilr/evilr.c
@@ -1,6 +1,8 @@
 #include <ruby.h> 
 
 static VALUE evilr_share_singleton_class(VALUE self, VALUE other) {
+  (void)rb_singleton_class(self);
+  (void)rb_singleton_class(other);
   ((struct RBasic*)self)->klass = ((struct RBasic*)other)->klass;
   return self;
 }
--- spec/evilr_spec.rb
+++ spec/evilr_spec.rb
@@ -7,6 +7,9 @@ describe "Object#share_singleton_class" do
     @o2 = Class.new.new
     def @o2.a; 1; end
   end
+  after do
+    GC.start # GC after spec to make sure nothing broke
+  end
 
   specify "should share singleton class with argument" do
     @o1.share_singleton_class(@o2)
@@ -15,5 +18,11 @@ describe "Object#share_singleton_class" do
     def @o2.c; b; end
     @o1.b.should == 2
     @o1.c.should == 2
+  end
+
+  specify "should create a singleton class if it doesn't exist" do
+    @o2.share_singleton_class(@o1)
+    def @o1.a; 3; end
+    @o2.a.should == 3
   end
 end
