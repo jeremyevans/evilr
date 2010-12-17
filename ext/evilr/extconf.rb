178b5dd4ee6605f6b594a6f24d69e18eac2a99ba
Jeremy Evans <code@jeremyevans.net>
2010-12-17 12:02:36 -0800

    Remove -DRUBY186, as I don't plan on testing or support 1.8.6

--- ext/evilr/extconf.rb
+++ ext/evilr/extconf.rb
@@ -1,6 +1,5 @@
 require 'mkmf'
 $CFLAGS << " -DRUBY19" if  RUBY_VERSION >= '1.9.0'
-$CFLAGS << " -DRUBY186" if  RUBY_VERSION < '1.8.7'
 $CFLAGS << " -Wall " unless RUBY_PLATFORM =~ /solaris/
 $CFLAGS << ' -g -ggdb -rdynamic -O0 -DDEBUG'
 $CFLAGS << " -Wconversion -Wsign-compare -Wno-unused-parameter -Wwrite-strings -Wpointer-arith -fno-common -pedantic -Wno-long-long"
