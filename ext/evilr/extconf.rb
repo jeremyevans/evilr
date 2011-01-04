ded2db2a9a5d4810da87001e5cc90db23f52f64c
Jeremy Evans <code@jeremyevans.net>
2011-01-04 11:28:04 -0800

    No longer compiler with DEBUG and STRICT by default
    Use the DEBUG and STRICT environment variables when compiling
    if you want that behavior.
    
    Also, no longer use -rdynamic when compiling with DEBUG, as
    gcc 4.4.5 seems to complain about it.

--- ext/evilr/extconf.rb
+++ ext/evilr/extconf.rb
@@ -1,7 +1,7 @@
 require 'mkmf'
 $CFLAGS << " -DRUBY19" if  RUBY_VERSION >= '1.9.0'
 $CFLAGS << " -Wall " unless RUBY_PLATFORM =~ /solaris/
-$CFLAGS << ' -g -ggdb -rdynamic -O0 -DDEBUG'
-$CFLAGS << " -Wconversion -Wsign-compare -Wno-unused-parameter -Wwrite-strings -Wpointer-arith -fno-common -pedantic -Wno-long-long"
+$CFLAGS << ' -g -ggdb -O0 -DDEBUG' if ENV['DEBUG']
+$CFLAGS << " -Wconversion -Wsign-compare -Wno-unused-parameter -Wwrite-strings -Wpointer-arith -fno-common -pedantic -Wno-long-long" if ENV['STRICT']
 $CFLAGS << (ENV['CFLAGS'] || '')
 create_makefile("evilr")
